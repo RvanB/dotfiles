30014e17f6f1d71d3c6b92bbef3a63e74f8d6d8d
RvanB <rvanbronkhorst@gmail.com>
2025-05-25 01:24:02 -0700

    Lots of improvements to emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,4 @@
-;; ========== Set up package archives ==========
+;;; Set up package archives 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
@@ -8,31 +8,31 @@
 (unless package-archive-contents
   (package-refresh-contents t))
 
-;; Disable lock files
+;;; Disable lock files
 (setq create-lockfiles nil)
 
-;; Only bring up warnings at error level
+;;; Only bring up warnings at error level
 (setq warning-minimum-level :error)
 
-;; Disable bell
+;;; Disable bell
 (setq ring-bell-function 'ignore)
 
-;; Don't show splash screen
+;;; Don't show splash screen
 (setq inhibit-startup-message t)
 
-;; Save history of minibuffer
+;;; Save history of minibuffer
 (setq history-length 25)
 (savehist-mode 1)
 
-;; Don't use my nice config for the custom variables
+;;; Don't use my nice config for the custom variables
 (setq custom-file (locate-user-emacs-file "custom-vars.el"))
 (load custom-file 'noerror 'nomessage)
 
-;; Auto revert EVERYTHING
+;;; Auto revert EVERYTHING
 (global-auto-revert-mode 1)
 (setq global-auto-revert-non-file-buffers t)
 
-;; Mac OS X Settings
+;;; Mac OS X Settings
 (when (string= system-type "darwin")       
   (setq dired-use-ls-dired nil))
 
@@ -41,44 +41,47 @@
   :config
   (exec-path-from-shell-initialize))
 
-;; Misc settings
+;;; Misc settings
 (setq make-backup-files nil)
 (setq vc-follow-symlinks t)
 
-;; Search settings
+;;; Search settings
 (setq isearch-lazy-count t)
 (setq lazy-count-prefix-format "(%s/%s) ")
 (setq lazy-count-suffix-format nil)
 (setq search-whitespace-regexp ".*?")
 
-;; Show line numbers in programming buffers
+;;; Show line numbers in programming buffers
 ;; (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
-;; Make it count lines for correct line number width
+;;; Make it count lines for correct line number width
 ;; (setq display-line-numbers-width-start t)
 
-;; ========== Terminals ==========
+;;; Terminals
 (use-package eat
   :ensure t)
 
-;; ========== Appearance ==========
+;;; Appearance
 
 (fringe-mode 0)
 
-(set-frame-parameter nil 'ns-appearance 'light)
-(set-frame-parameter nil 'ns-transparent-titlebar nil)
+;; (set-frame-parameter nil 'ns-appearance 'light)
+;; (set-frame-parameter nil 'ns-transparent-titlebar nil)
+
+(use-package ef-themes :ensure t)
+(load-theme 'ef-spring t)
 
 ;; (setq modus-themes-common-palette-overrides
 ;;       '((border-mode-line-active unspecified)
 ;;         (border-mode-line-inactive unspecified)))
 
-;; Add all your customizations prior to loading the themes
-(setq modus-themes-italic-constructs t
-      modus-themes-bold-constructs t)
+;;; Add all your customizations prior to loading the themes
+;; (setq modus-themes-italic-constructs t
+;;       modus-themes-bold-constructs t)
 
-(load-theme 'modus-operandi t)
+;; (load-theme 'modus-vivendi t)
 
-;; Change the color of the modeline
+;;; Change the color of the modeline
 ;; (set-face-foreground 'mode-line "#FFFFFF")
 ;; (set-face-background 'mode-line "#5E2A13")
 
@@ -86,19 +89,19 @@
 ;; (set-face-background 'line-number (modus-themes-get-color-value 'bg-main))
 ;; (set-face-foreground 'line-number (modus-themes-get-color-value 'blue-warmer))
 
-;; Disable menu bar
+;;; Disable menu bar
 (menu-bar-mode -1)
-;; Disable the scroll bar
+;;; Disable the scroll bar
 (scroll-bar-mode -1)
-;; Disable tool bar
+;;; Disable tool bar
 (tool-bar-mode -1)
 
-;; Set the font
+;;; Set the font
 (set-face-attribute 'default nil :font "Aporetic Sans Mono" :height 160)
 
-;; ========== Completions ==========
+;;; Completions
 
-;; Auto complete pairs (parentheses, brackets, etc.)
+;;; Auto complete pairs (parentheses, brackets, etc.)
 (electric-pair-mode 1)
 
 ;; (use-package icomplete
@@ -121,13 +124,116 @@
 
 (setq tab-always-indent 'complete)
 
-;; Detect indentation
+;;; Detect indentation
 (use-package dtrt-indent
   :ensure t)
 
 (dtrt-indent-global-mode t)
 
-;; Enable Vertico.
+;;; Consult
+(use-package consult
+  ;; Replace bindings. Lazily loaded by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Tweak the register preview for `consult-register-load',
+  ;; `consult-register-store' and the built-in commands.  This improves the
+  ;; register formatting, adds thin separator lines, register sorting and hides
+  ;; the window mode line.
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep consult-man
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+)
+
+;;; Enable Vertico.
 (use-package vertico
   :ensure t
   :custom
@@ -157,12 +263,18 @@
                  nil
                  (window-parameters (mode-line-format . none)))))
 
-;; Persist history over Emacs restarts. Vertico sorts by history position.
+;;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+;;; Persist history over Emacs restarts. Vertico sorts by history position.
 (use-package savehist
   :init
   (savehist-mode))
 
-;; Enable auto completion and configure quitting
+;;; Enable auto completion and configure quitting
 (setq corfu-auto t
       corfu-quit-no-match 'separator) ;; or t
 
@@ -178,7 +290,7 @@
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion)))))
 
-;; ========== Tree sitter ==========
+;;; Tree sitter
 ;; (use-package tree-sitter
 ;;   :ensure t)
 
@@ -234,17 +346,17 @@
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
-;; ========== PDFs ==========
+;;; PDFs
 (use-package pdf-tools
   :ensure t
   :config
   (setq pdf-view-use-scaling nil))
 
-;; ========== Magit ==========
+;;; Magit
 (use-package magit
   :ensure t)
 
-;; ========== LSP ==========
+;;; LSP
 ;; (use-package lsp-mode
 ;;   :ensure t
 ;;   :init
@@ -268,17 +380,36 @@
   :hook
   (prog-mode . eglot-ensure))
 
-;; ========== Which key ==========
+;;; Which key
 (use-package which-key
   :ensure t
   :config
   (which-key-mode))
 
-;; ========== Text editing improvements ==========
+;;; Text editing improvements
 (use-package expand-region
   :ensure t
   :bind
-  ("C-=" . er/expand-region))
+  ("C-=" . er/expand-region)
+  :config
+  (setq expand-region-smart-cursor t)
+  ;; Prioritize symbol over word
+  (setq expand-region-exclude-text-mode-expansions nil)
+  (setq er/try-expand-list
+        '(er/mark-symbol
+          er/mark-method-call
+          er/mark-inside-pairs
+          er/mark-outside-pairs
+          er/mark-inside-quotes
+          er/mark-outside-quotes
+          er/mark-comment
+          er/mark-defun
+          er/mark-url
+          er/mark-email
+          er/mark-text-paragraph
+          er/mark-text-sentence
+          er/mark-paragraph
+          er/mark-page)))
 
 (use-package smart-hungry-delete
   :ensure t
@@ -287,7 +418,35 @@
 	 ([remap delete-char] . smart-hungry-delete-forward-char))
   :init (smart-hungry-delete-add-default-hooks))-000
 
-;; ========== Python stuff ==========
+;;; Remap M-f and M-b to move by symbol instead of word
+;; (global-set-key (kbd "M-f") #'forward-symbol)
+;; (global-set-key (kbd "M-b") (lambda () (interactive) (forward-symbol -1)))
+
+;;; Surround
+(use-package surround
+  :ensure t
+  :bind-keymap ("M-'" . surround-keymap))
+
+;;; Delete selection on type
+(delete-selection-mode 1)
+
+;; Flymake ruff
+(use-package flymake-ruff
+  :ensure t
+  :hook (eglot-managed-mode . flymake-ruff-load))
+
+;; This function isn't working for some reason, provided by the flymake-ruff git repo
+;; (defun rvb/filter-eglot-diagnostics (diags)
+;;     "Drop Pyright 'variable not accessed' notes from DIAGS."
+;;     (list (seq-remove (lambda (d)
+;;                         (and (eq (flymake-diagnostic-type d) 'eglot-note)
+;;                              (s-starts-with? "Pyright:" (flymake-diagnostic-text d))
+;;                              (s-ends-with? "is not accessed" (flymake-diagnostic-text d))))
+;;                       (car diags))))
+
+;; (advice-add 'eglot--report-to-flymake :filter-args #'rvb/filter-eglot-diagnostics)
+
+;;; Pyright configuration
 (defun rvb/pyright-config ()
   "Create a JSON configuration file for Python using a specified package manager to find the venv."
   (interactive)
@@ -335,13 +494,13 @@
   (let ((output-buffer (get-buffer-create "*ruff-check*")))
 	(with-current-buffer output-buffer
 	  (erase-buffer)
-	  (insert (shell-command-to-string (format "NO_COLOR=1 ruff check -q %s" (when-let ((project (project-current))) (project-root project)))))
+	  (insert (shell-command-to-string (format "NO_COLOR=1 ruff check --output-format=concise -q %s" (when-let ((project (project-current))) (project-root project)))))
 	  (compilation-mode)
 	  (local-set-key "q" (lambda () (interactive) (quit-window t))))
 	(display-buffer output-buffer)))
 (global-set-key (kbd "C-c r") 'rvb/ruff-check-project)
 
-;; ========== MRK Mode ==========
+;; MRK Mode
 ;; Create a major mode for .mrk files called MRK
 (define-derived-mode mrk-mode text-mode "MRK"
   "Major mode for editing .mrk files."
@@ -359,8 +518,8 @@
 (add-to-list 'eglot-server-programs '(mrk-mode . ("/Users/rvanbron/test-lsp/.venv/bin/python" "/Users/rvanbron/test-lsp/test.py")))
 (add-hook 'mrk-mode-hook 'eglot-ensure)
 
-;; ========== Window management ==========
-;; Enable keybindings for window switching
+;;; Window management
+;;; Enable keybindings for window switching
 (windmove-default-keybindings)
 
 (defun rvb/kill-buffer-and-close-window ()
@@ -374,23 +533,23 @@
                (not (window-live-p (get-buffer-window buffer))))
       (delete-window))))
 
-;; Killing a window closes the buffer
+;;; Killing a window closes the buffer
 (global-set-key (kbd "s-k") 'rvb/kill-buffer-and-close-window)
 
-;; Window traversal
+;;; Window traversal
 (defun rvb/other-window-backward (&optional n)
   "Select Nth previous window."
   (interactive "P")
   (other-window (- (prefix-numeric-value n))))
 
-;; Bindings for forward and backward
+;;; Bindings for forward and backward
 (global-set-key (kbd "C-c n") 'other-window)
 (global-set-key (kbd "C-c p") 'rvb/other-window-backward)
 
-;; Enable narrowing
+;;; Enable narrowing
 (put 'narrow-to-region 'disabled nil)
 
-;; ========== In-Buffer Movement ==========
+;;; In-Buffer Movement
 (use-package ultra-scroll
   :pin "manual"
   :vc (:url "https://github.com/jdtsmith/ultra-scroll"
@@ -402,7 +561,7 @@
   :config
   (ultra-scroll-mode 1))
 
-;; Disable changing text scale with the mouse
+;;; Disable changing text scale with the mouse
 (global-set-key (kbd "<pinch>") 'ignore)
 (global-set-key (kbd "<C-wheel-up>") 'ignore)
 (global-set-key (kbd "<C-wheel-down>") 'ignore)
@@ -414,7 +573,7 @@
 (global-set-key [remap move-beginning-of-line] 'rvb/back-to-indentation-or-beginning)
 (global-set-key [remap org-beginning-of-line] 'rvb/back-to-indentation-or-beginning)
 
-;; ========== Search Improvements ==========
+;;; i-search changes
 ;; https://emacs.stackexchange.com/questions/53004/improving-isearch/53006#53006
 (defun rvb/isearch-repeat-forward+ ()
   (interactive)
@@ -433,14 +592,15 @@
   (unless isearch-success
     (isearch-repeat-backward)))
 
+
 (keymap-set isearch-mode-map "C-s" 'rvb/isearch-repeat-forward+)
 (keymap-set isearch-mode-map "C-r" 'rvb/isearch-repeat-backward+)
 
-;; ========== Org mode ==========
+;;; Org mode
 (setq org-directory "~/orgfiles/")
 (setq org-agenda-files (list org-directory))
 
-;; ========== AI Tools ==========
+;;; gptel
 (use-package gptel
   :ensure t
   :config
@@ -451,6 +611,7 @@
   :bind
   (("C-c g m" . gptel-menu)))
 
+;;; GitHub Copilot
 (use-package copilot
   :vc (:url "https://github.com/copilot-emacs/copilot.el"
             :rev :newest
