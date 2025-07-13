ea6d55054358d2cce9d7bb499d7aee768c694425
RvanB <rvanbronkhorst@gmail.com>
2025-07-13 00:52:16 -0700

    Use pixel scroll mode for keyboard scroll

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -797,6 +797,64 @@
   :config
   (ultra-scroll-mode 1))
 
+;; https://www.reddit.com/r/emacs/comments/1g092xp/hack_use_pixelscroll_for_all_scrolling_and/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+(defun kb/pixel-recenter (&optional arg redisplay)
+  "Similar to `recenter' but with pixel scrolling.
+ARG and REDISPLAY are identical to the original function."
+  ;; See the links in line 6676 in window.c for
+  (when-let* ((current-pixel (pixel-posn-y-at-point))
+              (target-pixel (if (numberp arg)
+                                (* (line-pixel-height) arg)
+                              (* 0.5 (window-body-height nil t))))
+              (distance-in-pixels 0)
+              (pixel-scroll-precision-interpolation-total-time
+               (/ pixel-scroll-precision-interpolation-total-time 2.0)))
+    (setq target-pixel
+          (if (<= 0 target-pixel)
+              target-pixel
+            (- (window-body-height nil t) (abs target-pixel))))
+    (setq distance-in-pixels (- target-pixel current-pixel))
+    (condition-case err
+        (pixel-scroll-precision-interpolate distance-in-pixels nil 1)
+      (error (message "[kb/pixel-recenter] %s" (error-message-string err))))
+    (when redisplay (redisplay t))))
+
+(defun kb/pixel-scroll-up (&optional arg)
+  "(Nearly) drop-in replacement for `scroll-up'."
+  (cond
+   ((eq this-command 'scroll-up-line)
+    (funcall (ad-get-orig-definition 'scroll-up) (or arg 1)))
+   (t
+    (unless (eobp) ; Jittery window if trying to go down when already at bottom
+      (pixel-scroll-precision-interpolate
+       (- (* (line-pixel-height)
+             (or arg (- (window-text-height) next-screen-context-lines))))
+       nil 1)))))
+
+(defun kb/pixel-scroll-down (&optional arg)
+  "(Nearly) drop-in replacement for `scroll-down'."
+  (cond
+   ((eq this-command 'scroll-down-line)
+    (funcall (ad-get-orig-definition 'scroll-down) (or arg 1)))
+   (t
+    (pixel-scroll-precision-interpolate
+     (* (line-pixel-height)
+        (or arg (- (window-text-height) next-screen-context-lines)))
+     nil 1))))
+
+(add-hook 'ultra-scroll-mode-hook
+          (lambda ()
+            (cond
+             (pixel-scroll-precision-mode
+              (advice-add 'scroll-up-command :override 'kb/pixel-scroll-up)
+              (advice-add 'scroll-down-command :override 'kb/pixel-scroll-down)
+              (advice-add 'recenter-top-bottom :override 'kb/pixel-recenter))
+             (t
+              (advice-remove 'scroll-up-command 'kb/pixel-scroll-up)
+              (advice-remove 'scroll-down-command 'kb/pixel-scroll-down)
+              (advice-remove 'recenter-top-bottom 'kb/pixel-recenter)))))
+
+
 ;;; Disable changing text scale with the mouse
 (global-set-key (kbd "<pinch>") 'ignore)
 (global-set-key (kbd "<C-wheel-up>") 'ignore)
