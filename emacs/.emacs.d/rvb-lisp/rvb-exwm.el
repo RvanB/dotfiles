6fe374ab30e85978eba2e4b690bf9824314033b6
RvanB <rvanbronkhorst@gmail.com>
2025-08-16 14:14:21 -0700

    Add printscreen and better app launcher

--- emacs/.emacs.d/rvb-lisp/rvb-exwm.el
+++ emacs/.emacs.d/rvb-lisp/rvb-exwm.el
@@ -10,20 +10,25 @@
 (add-hook 'exwm-update-class-hook
 	  (lambda () (exwm-workspace-rename-buffer exwm-class-name)))
 
+(defun rvb/launch-app () ;; s-&: Launch application with Vertico completion
+  (interactive)
+  (let* ((cmd (completing-read "Shell command: "
+                               (split-string (shell-command-to-string "whence -pm '*'") "\n" t)
+                               nil t)))
+    (start-process-shell-command cmd nil cmd)))
+
 ;; Global keybindings
 (setq exwm-input-global-keys
       `(([?\s-r] . exwm-reset) ;; s-r: Reset (to line-mode)
-	([?\s-w] . exwm-workspace-switch) ;; s-w: Switch workspace
-	([?\s-&] . (lambda (cmd) ;; s-&: Launch application
-		     (interactive (list (read-shell-command "$ ")))
-		     (start-process-shell-command cmd nil cmd)))
-	;; s-N: Switch to certain workspace
-	,@(mapcar (lambda (i)
-		    `(,(kbd (format "s-%d" i)) .
-		      (lambda ()
-			(interactive)
-			(exwm-workspace-switch-create ,i))))
-		  (number-sequence 0 9))))
+        ([?\s-w] . exwm-workspace-switch) ;; s-w: Switch workspace
+        ([?\s-&] . rvb/launch-app) ;; Non-blocking start
+        ;; s-N: Switch to certain workspace
+        ,@(mapcar (lambda (i)
+                   `(,(kbd (format "s-%d" i)) .
+                     (lambda ()
+                       (interactive)
+                       (exwm-workspace-switch-create ,i))))
+                 (number-sequence 0 9))))
 
 ;; Enable EXWM
 (add-hook 'after-init-hook 'exwm-wm-mode)
@@ -50,6 +55,16 @@
    (lambda ()
      (interactive)
      '(pulseaudio-control-toggle-current-sink-mute)))
+
+;; Print screen
+(global-set-key (kbd "<print>")
+  (lambda ()
+    (interactive)
+    (let ((path (concat "~/Documents/Screenshot-" (format-time-string "%Y-%m-%d,%H:%M:%S") ".png")))
+      (start-process-shell-command
+       "import" nil (concat "import -window root " path))
+    (message (concat "Screenshot saved to " path)))
+    ))
 
 (provide 'rvb-exwm)
 source
