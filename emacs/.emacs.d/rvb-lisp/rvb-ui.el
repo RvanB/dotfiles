0c6df0b333eba75c963bd546e8b89e2db40a1fbf
RvanB <rvanbronkhorst@gmail.com>
2025-12-02 08:27:06 -0800

    Switch back to Berkeley Mono

--- emacs/.emacs.d/rvb-lisp/rvb-ui.el
+++ emacs/.emacs.d/rvb-lisp/rvb-ui.el
@@ -56,8 +56,8 @@
 (add-hook 'after-make-frame-functions
 		  (lambda (frame)
 			(with-selected-frame frame
-			  (set-face-attribute 'default nil :font "NanumGothicCoding 18"))))
-(set-face-attribute 'default nil :font "NanumGothicCoding 18")
+			  (set-face-attribute 'default nil :font "Berkeley Mono Variable Z8XX46Z7 14"))))
+(set-face-attribute 'default nil :font "Berkeley Mono Variable Z8XX46Z7 14")
 
 ;; Ligatures
 (use-package ligature
