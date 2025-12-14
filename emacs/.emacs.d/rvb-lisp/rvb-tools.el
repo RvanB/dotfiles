f006f77902502e1b05243905d5c3e35d7073c0c0
RvanB <rvanbronkhorst@gmail.com>
2025-12-13 22:48:54 -0800

    feat(dired): Add mouse bindings to open files and toggle marks
    - Bind mouse-2 to dired-mouse-find-file to open files/directories
    - Add rvb/dired-mouse-mark and bind mouse-3 to toggle marks in dired

--- emacs/.emacs.d/rvb-lisp/rvb-tools.el
+++ emacs/.emacs.d/rvb-lisp/rvb-tools.el
@@ -18,4 +18,17 @@
 (use-package dap-mode
   :ensure t)
 
+(define-key dired-mode-map [mouse-2] 'dired-mouse-find-file)
+
+(defun rvb/dired-mouse-mark (event)
+  "Toggle mark at mouse click."
+  (interactive "e")
+  (mouse-set-point event)
+  (save-excursion
+    (beginning-of-line)
+    (if (eq (char-after) dired-marker-char)
+        (dired-unmark 1)
+      (dired-mark 1))))
+(define-key dired-mode-map [mouse-3] 'rvb/dired-mouse-mark)
+
 (provide 'rvb-tools)
