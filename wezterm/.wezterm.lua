f575d3ae142236f85a0422c9fb1da45e0e983416
RvanB <rvanbronkhorst@gmail.com>
2026-04-28 19:30:25 -0700

    Reenable treesit auto and customize wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,6 +4,24 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.use_fancy_tab_bar = false
+
+config.colors = {
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = '#ffffff',
+    new_tab = {
+        bg_color = '#ffffff',
+        fg_color = '#000000',
+    },
+    new_tab_hover = {
+        bg_color = '#ffffff',
+        fg_color = '#000000',
+        italic = false,
+    },
+  },
+}
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
@@ -13,11 +31,47 @@ config.initial_rows = 28
 -- or, changing the font size and color scheme.
 config.font_size = 14
 config.color_scheme = 'Modus-Operandi'
-config.use_fancy_tab_bar = false
+
 config.font = wezterm.font(
     'Berkeley Mono Variable Z8XX46Z7',
     { weight = 'Regular' }
 )
+
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+
+tabline.setup({
+  options = {
+    icons_enabled = true,
+    theme = 'Modus-Operandi',
+    tabs_enabled = true,
+    theme_overrides = {
+        normal_mode = {
+            f = { bg = '#ff0000' },
+        },
+    },
+    section_separators = '',
+    component_separators= '',
+    tab_separators = '',
+  },
+  sections = {
+    tabline_a = {},
+    tabline_b = {},
+    tabline_c = {},
+    tab_active = {
+      'index',
+      { 'parent', padding = 0 },
+      '/',
+      { 'cwd', padding = { left = 0, right = 1 } },
+      { 'zoomed', padding = 0 },
+    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+    tabline_x = { 'ram', 'cpu' },
+    tabline_y = { 'datetime' },
+    tabline_z = { 'domain' },
+  },
+  extensions = {},
+})
+
 
 config.keys = {
   -- Navigate panes with ALT + hjkl
